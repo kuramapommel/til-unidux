4c15d89244f81307bc7ffa42eaa00c1b102c6751
kuramapommel <pommel.pg@gmail.com>
2020-02-29 23:45:41 +0900

    ゲームセット後にタップするとタイトルに戻るように実装

---
+++ Assets/Reversi/Script/Main/Presentation/Scene/InGame/ResultDispatcher.cs
@@ -0,0 +1,25 @@
+﻿using Pommel.Reversi.Presentation.Project.SceneChange;
+using Unidux.SceneTransition;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using ProjectUnidux = Pommel.Reversi.Presentation.Project.Unidux;
+using SceneType = Pommel.Reversi.Presentation.Project.SceneChange.Scene;
+
+namespace Pommel.Reversi.Presentation.Scene.InGame
+{
+    public sealed class ResultDispatcher : MonoBehaviour
+    {
+        [SerializeField]
+        private Button m_button;
+
+        private void OnEnable()
+        {
+            _ = m_button.OnClickAsObservable()
+                .TakeUntilDisable(this)
+                .Select(_ => PageDuck<Page, SceneType>.ActionCreator.Push(Page.TitlePage))
+                .Subscribe(action => ProjectUnidux.Dispatch(action))
+                .AddTo(this);
+        }
+    }
+}
