9ad69cc6443ee0d4e6ede6dbaf427b2bd4daf70b
kuramapommel <pommel.pg@gmail.com>
2020-03-02 23:08:19 +0900

    RuntimeInitializeOnLoadMethod を利用したゲームの初期化処理

--- Assets/Reversi/Script/Main/Presentation/Project/SceneRoot.cs
+++ Assets/Reversi/Script/Main/Presentation/Project/SceneRoot.cs
@@ -1,13 +1,15 @@
-﻿using Unidux.SceneTransition;
+﻿using Unidux;
+using Unidux.SceneTransition;
 using UnityEngine;
 using Page = Pommel.Reversi.Presentation.Project.SceneChange.Page;
 using SceneType = Pommel.Reversi.Presentation.Project.SceneChange.Scene;
 
 namespace Pommel.Reversi.Presentation.Project
 {
-    public sealed class SceneDispatcher : MonoBehaviour
+    public sealed class SceneRoot : SingletonMonoBehaviour<SceneRoot>
     {
-        void Awake()
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void InitializeGame()
         {
             Unidux.Dispatch(PageDuck<Page, SceneType>.ActionCreator.Reset());
             Unidux.Dispatch(PageDuck<Page, SceneType>.ActionCreator.Push(Page.TitlePage));
