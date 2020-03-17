a22c88314c5655e6ff95e0d3ddf82f6e511a2552
kuramapommel <pommel.pg@gmail.com>
2020-03-17 14:10:07 +0900

    とりあえずmiddlewareのガラを作成

---
+++ Assets/Reversi/Script/Main/Presentation/Scene/InGame/Middlewares.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Unidux;
+
+namespace Pommel.Reversi.Presentation.Scene.InGame
+{
+    public static class Middlewares
+    {
+        public static Func<Func<object, object>, Func<object, object>> ChangeTurn(IStoreObject store)
+        {
+            return (Func<object, object> next) => (object action) =>
+            {
+                return next(action);
+            };
+        }
+
+        public static Func<Func<object, object>, Func<object, object>> Result(IStoreObject store)
+        {
+            return (Func<object, object> next) => (object action) =>
+            {
+                return next(action);
+            };
+        }
+    }
+}
