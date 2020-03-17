2ccc816f037a9c276314f0fae038e92b85f5f63d
kuramapommel <pommel.pg@gmail.com>
2020-03-17 13:59:15 +0900

    middleware 側でやるべき処理を一旦コメントアウト

--- Assets/Reversi/Script/Main/Presentation/Scene/InGame/StoneAction.cs
+++ Assets/Reversi/Script/Main/Presentation/Scene/InGame/StoneAction.cs
@@ -39,42 +39,41 @@ namespace Pommel.Reversi.Presentation.Scene.InGame
 
                         if (!state.Stones.CanPut(action.X, action.Y, state.Turn.IsBlackTurn)) return state;
 
-                        var stone = state.Stones[action.X][action.Y];
-                        var isBlackTurn = state.Turn.IsBlackTurn;
+                        state.Stones.Flip(action.X, action.Y, state.Turn.IsBlackTurn);
 
-                        state.Stones.Flip(action.X, action.Y, isBlackTurn);
-                        var (canAgainPut, canOpponentPut) = isBlackTurn
-                            ? (state.Stones.CanPutBalck, state.Stones.CanPutWhite)
-                            : ((Func<bool>)state.Stones.CanPutWhite, (Func<bool>)state.Stones.CanPutBalck);
+                        // todo 以下の部分を middleware に移管する
+                        //var (canAgainPut, canOpponentPut) = isBlackTurn
+                        //    ? (state.Stones.CanPutBalck, state.Stones.CanPutWhite)
+                        //    : ((Func<bool>)state.Stones.CanPutWhite, (Func<bool>)state.Stones.CanPutBalck);
 
-                        if (canOpponentPut())
-                        {
-                            state.Turn.IsBlackTurn = !isBlackTurn;
-                            return state;
-                        }
+                        //if (canOpponentPut())
+                        //{
+                        //    state.Turn.IsBlackTurn = !isBlackTurn;
+                        //    return state;
+                        //}
 
-                        if (canAgainPut()) return state;
+                        //if (canAgainPut()) return state;
 
-                        var (black, white) = state.Stones
-                            .SelectMany(stones => stones)
-                            .Aggregate(
-                                (blackCount: 0, whiteCount: 0),
-                                (aggregate, element) =>
-                                {
-                                    switch (element.Color)
-                                    {
-                                        case StoneStateElement.State.Black: return (aggregate.blackCount + 1, aggregate.whiteCount);
-                                        case StoneStateElement.State.White: return (aggregate.blackCount, aggregate.whiteCount + 1);
-                                    }
+                        //var (black, white) = state.Stones
+                        //    .SelectMany(stones => stones)
+                        //    .Aggregate(
+                        //        (blackCount: 0, whiteCount: 0),
+                        //        (aggregate, element) =>
+                        //        {
+                        //            switch (element.Color)
+                        //            {
+                        //                case StoneStateElement.State.Black: return (aggregate.blackCount + 1, aggregate.whiteCount);
+                        //                case StoneStateElement.State.White: return (aggregate.blackCount, aggregate.whiteCount + 1);
+                        //            }
 
-                                    return aggregate;
-                                });
+                        //            return aggregate;
+                        //        });
 
-                        state.Result.Winner = (white > black)
-                            ? WinnerStateElement.State.White
-                            : (black > white)
-                                ? WinnerStateElement.State.Black
-                                : WinnerStateElement.State.Draw;
+                        //state.Result.Winner = (white > black)
+                        //    ? WinnerStateElement.State.White
+                        //    : (black > white)
+                        //        ? WinnerStateElement.State.Black
+                        //        : WinnerStateElement.State.Draw;
                         return state;
 
                     default: throw new ArgumentOutOfRangeException();
