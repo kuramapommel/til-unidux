a345f502b8789972c6ebe043508ed22dc483fe15
kuramapommel <pommel.pg@gmail.com>
2020-05-02 15:57:37 +0900

    prefab factory

--- Assets/Reversi/Script/Main/Installer/Scene/InGame/GameInstaller.cs
+++ Assets/Reversi/Script/Main/Installer/Scene/InGame/GameInstaller.cs
@@ -8,20 +8,33 @@ using Pommel.Reversi.UseCase.InGame;
 using Pommel.Reversi.UseCase.InGame.Dto;
 using Pommel.Reversi.UseCase.Shared;
 using UniRx.Async;
+using UnityEngine;
 using Zenject;
+using _Color = Pommel.Reversi.Domain.InGame.Color;
+using _Stone = Pommel.Reversi.Presentation.Scene.InGame.View.Stone;
 
 namespace Pommel.Reversi.Installer.Scene.Ingame
 {
     public sealed class GameInstaller : MonoInstaller
     {
+        [SerializeField]
+        private _Stone m_stonePrefab;
+
+        [SerializeField]
+        private RectTransform m_stoneParent;
+
         public override void InstallBindings()
         {
             // factories
             Container.BindIFactory<IEnumerable<IStoneState>, IGameBoardState>().To<GameBoardState>().AsCached();
-            Container.BindIFactory<Point, Color, IStoneState>().To<StoneState>().AsCached();
+            Container.BindIFactory<Point, _Color, IStoneState>().To<StoneState>().AsCached();
             Container.BindIFactory<IGameRepository, IEventPublisher, string, IPutStoneUseCase>().To<PutStoneUseCase>().AsCached();
             Container.BindIFactory<IEventBroker, IEventPublisher>(); // todo concrete class 実装する
             Container.BindIFactory<Func<ResultDto, UniTask>, Func<PuttedDto, UniTask>, IGameResultService, IEventSubscriber>().To<PuttedStoneEventSubscriber>().AsCached();
+            Container.BindIFactory<IStoneState, IStone>().To<_Stone>()
+                .FromComponentInNewPrefab(m_stonePrefab)
+                .UnderTransform(m_stoneParent)
+                .AsCached();
 
             // repositories
             Container.BindInterfacesTo<IGameRepository>().AsCached(); // todo concrete class 実装する
