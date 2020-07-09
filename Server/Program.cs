8e9bfca71f811f51156d593562594e0e97dc7245
kuramapommel <pommel.pg@gmail.com>
2020-07-09 13:35:02 +0900

    server 側の DI

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.Extensions.Hosting;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Grpc.Core;
 using MagicOnion.Hosting;
 using MagicOnion.Server;
-using Grpc.Core;
 using Microsoft.Extensions.DependencyInjection;
-using Pommel.Server.UseCase.InGame;
+using Microsoft.Extensions.Hosting;
 using Pommel.Server.Domain.InGame;
+using Pommel.Server.Infrastructure.Repository.InGame;
+using Pommel.Server.Infrastructure.Store.InGame;
+using Pommel.Server.UseCase.InGame;
 
 namespace Pommel.Server
 {
@@ -29,12 +31,39 @@ namespace Pommel.Server
                 {
                     // todo 分割しないとやばいことになるので分割する
                     // dependency injection
+                    services.AddSingleton(GameStore.Instance);
+                    services.AddSingleton(GameResultStore.Instance);
+                    services.AddSingleton(LaidResultStore.Instance);
+                    services.AddSingleton(MatchingStore.Instance);
+
+                    services.AddSingleton<IPlayerFactory, PlayerFactory>();
+                    services.AddSingleton<IMatchingFactory, MatchingFactory>();
+                    services.AddSingleton<IGameFactory, GameFactory>();
+
+                    services.AddSingleton<IMatchingRepository, MatchingRepository>();
+                    services.AddSingleton<IGameRepository, GameRepository>();
+
                     services.AddSingleton<IStartGameUseCase, StartGameUseCase>();
                     services.AddSingleton<ILayPieceUseCase, LayPieceUseCase>();
                     services.AddSingleton<ICreateMatchingUseCase, CreateMatchingUseCase>();
                     services.AddSingleton<ICreateGameUseCase, CreateGameUseCase>();
                 })
                 .RunConsoleAsync();
+        }
+
+        private sealed class PlayerFactory : IPlayerFactory
+        {
+            public IPlayer Create(string id, string name) => new Player.Impl(id, name);
+        }
+
+        private sealed class MatchingFactory : IMatchingFactory
+        {
+            public IMatching Create(string id, IPlayer first) => new Matching(id, first);
+        }
+
+        private sealed class GameFactory : IGameFactory
+        {
+            public IGame Create(string id, string resultId, string matchingId) => new Game(id, resultId, matchingId);
         }
     }
 }
